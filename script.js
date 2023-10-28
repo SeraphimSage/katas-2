8254c22620376d0ecb110bea737a03bdb91fe75b
Kenneth Pinkerton <kenneth.pinkerton90@gmail.com>
2023-10-27 21:31:32 -0500

    Update script.js
    Update to functions to write to the document and be visible to user.

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-function add(x,y){
-    return x + y;    
+function add(x, y) {
+	return x + y;
 }
-console.log(add(2,4))
+document.write("<h2>Addition function:<br/></h2>");
+document.write(2 + " + " + 4 + " = ");
+document.write(add(2, 4));
 
-function multiply(x,y){
-    return x * y;
+function multiply(x, y) {
+	return x * y;
 }
-console.log(multiply(6,8))
+document.write("<h2>Multiplication function:<br/></h2>");
+document.write(6 + " * " + 8 + " = ");
+document.write(multiply(6, 8));
 
-function power(x,n){
-    var answer = 1;
+function power(x, n) {
+	var answer = 1;
 
-    for(let i = 0; i < n; i ++){
-        answer = multiply(answer,x)
-    }
-    return answer
+	for (let i = 0; i < n; i++) {
+		answer = multiply(answer, x);
+	}
+	return answer;
 }
-console.log(power(2,8))
+document.write("<h2>Powers function:<br/></h2>");
+document.write(2 + " ^ " + 8 + " = ");
+document.write(power(2, 8));
 
-function factorial(x){
-    answer1 = 1;
+function factorial(x) {
+	answer1 = 1;
 
-    for(let i = 1; i <= x; i ++){
-        answer1 = multiply(answer1,i)
-    }
-    return answer1
+	for (let i = 1; i <= x; i++) {
+		answer1 = multiply(answer1, i);
+	}
+	return answer1;
 }
-console.log(factorial(4))
+document.write("<h2>Factorial function:<br/></h2>");
+document.write(4 + "! " + " = ");
+document.write(factorial(4));
 
-function fibonacci(x){
-    var answer2 = x;
-    answer2 = add(answer2 - 1,answer2 - 2)
-    return answer2
+function fibonacci(x) {
+	var answer2 = x;
+	answer2 = add(answer2 - 1, answer2 - 2);
+	return answer2;
 }
-console.log(fibonacci(8))
+document.write("<h2>Fibonacci function:<br/></h2>");
+document.write("(8 - 1) + (8 - 2) =");
+document.write(fibonacci(8));
